607fa3c61762bee2780ffc393a93b82aa5b36554
Marcielle <marcielle9908@gmail.com>
2022-06-13 14:36:23 -0300

    Segundo exercicio do dia 4.1

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/index.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/index.js
@@ -1,3 +1,5 @@
+//PRIMEIRO EXERCICIO DO BLOCO 4
+
 const myName = "Marcielle";
 const birthCity = "Belo Horizonte";
 let birthYear = "1997";
@@ -7,3 +9,25 @@ birthCity = "Porto Seguro";
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
+
+//SEGUNDO EXERCICIO DO BLOCO 4
+
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge)
+
+const base = 5;
+let height = 8;
+const area  = (base * height);
+console.log(area);
+const perimeter = 40;
