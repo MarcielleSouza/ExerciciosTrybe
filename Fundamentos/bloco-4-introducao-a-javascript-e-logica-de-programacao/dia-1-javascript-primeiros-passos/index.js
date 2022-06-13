2852ca416919de171892fe7f9384903461de7658
Marcielle <marcielle9908@gmail.com>
2022-06-13 13:11:58 -0300

    Primeiro exercicio do dia 4.1

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/index.js
@@ -0,0 +1,9 @@
+const myName = "Marcielle";
+const birthCity = "Belo Horizonte";
+let birthYear = "1997";
+birthYear = 2030;
+birthCity = "Porto Seguro";
+//Aparecera um mensagem de erro na linha 5 pois não da para alterar o valor de uma constante.
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
